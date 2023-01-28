ec536bbce2abf213b671a720ca40b95afe255f6b
grinya0101 <grinya1994015@gmail.com>
2023-01-28 07:08:44 +0300

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
+
+const container = document.querySelector(".gallery");
+createGelaryCard(galleryItems);
+
+container.addEventListener("click", onConteinerClick);
+
+function onConteinerClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}">
+`);
+
+  instance.show();
+}
+
+function createGelaryCard(imgs) {
+  const marcup = imgs
+    .map(({ original, preview }) => {
+      return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="Image description"
+          />
+        </a>
+      </div> `;
+    })
+    .join("");
+
+  container.insertAdjacentHTML("beforeend", marcup);
+}
